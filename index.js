c6a5c246c23697f3f7d0570dbce2406a3cf0a812
Juanma <juanmafallo@gmail.com>
2021-08-25 17:49:30 -0300

    ..

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-const app = require("express");
+const express = require("express");
+const app = express();
 const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer);
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: ["http://localhost/4000"]
+  }
+});
+const handlebars = require("express-handlebars");
 
-const PORT = 8080;
+const PORT = 4000;
+
+io.on("connection", (socket) => {
+  console.log("Usuario conectado");
+});
 
 app.use(express.json);
 app.use(express.urlencoded({ extended: true }));
-app.engine("hbs", {
-  extname: ".hbs",
-  defaultLayout: "index.hbs",
-  layoutsDir: __dirname + "/views/layouts",
-  partialsDir: __dirname + "/views/partials"
-});
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials"
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.send("home");
 });
 
-
-
-httpServer.listen(PORT, "Servidor activo en puerto: " + port);
+httpServer.listen(PORT, () =>
+  console.log("Servidor activo en puerto: " + PORT)
+);
